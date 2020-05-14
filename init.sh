78825831d77f899541e9450fe53289e9c68a6965
huxy11 <huxy11@yeah.net>
2020-05-14 09:14:18 +0800

    refine init.sh

--- init.sh
+++ init.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-version=ics2019
-
-log=""
-
 function init() {
-  if [ -d $1 ]; then
-    echo "$1 is already initialized, exiting..."
-    return
-  fi
-
-  while [ ! -d $1 ]; do
-    git clone -b $version https://github.com/NJU-ProjectN/$1.git
-  done
-  log="$log$1 `cd $1 && git log --oneline --no-abbrev-commit -n1`"$'\n'
-  rm -rf $1/.git
-
-  if [ $2 ] ; then
-    sed -i -e "/^export $2=.*/d" ~/.bashrc
-    echo "export $2=`readlink -e $1`" >> ~/.bashrc
+  if [ $1 ] ; then
+    sed -i -e "/^export $1=.*/d" ~/.bashrc
+    echo "export $1=`readlink -e $1`" >> ~/.bashrc
   fi
 }
 
-read -r -p "Are you sure to initialize everything? [y/n] " input
+read -r -p "Are you sure to initialize evns ? [y/n] " input
 case $input in
   [yY])
-    init nemu NEMU_HOME
-    init nexus-am AM_HOME
-    init nanos-lite
-    init navy-apps NAVY_HOME
-
-    git add -A
-    git commit -am "$version initialized"$'\n\n'"$log"
+    init NEMU_HOME
+    init AM_HOME
+    init NAVY_HOME
 
     echo "Initialization finishes!"
     echo "By default this script will add environment variables into ~/.bashrc."
